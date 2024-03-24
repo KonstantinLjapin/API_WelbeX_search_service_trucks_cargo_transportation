28fd7a915562f3a1d2a61c7837cca8d998a12a25
lyapin <lajpinkonstantin@gmail.com>
2024-03-24 21:03:49 +0300

    polish_start_project

--- api_app/start.sh
+++ api_app/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # need chmod +
-sleep 10s
-cd api_app/
-uvicorn main:app --host 0.0.0.0;
+sleep 10s;
+
+uvicorn api_app.main:app --host 0.0.0.0;
--- start_test.sh
+++ start_test.sh
@@ -5,3 +5,4 @@ sudo docker stop $(sudo docker ps -a -q);
 sudo docker rm $(sudo docker ps -a -q);
 sudo docker rmi $(sudo docker images --format="{{.Repository}} {{.ID}}" |
                   grep "^api_welbex_search_service_trucks_cargo_transportation-api_fast_api" | cut -d' ' -f2);
+sudo rm -r dump/;
